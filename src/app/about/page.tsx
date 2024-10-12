e1879df9559c49af2f80613388124f9492e0188d
ddcsoftdev <ddcsoftwaredev@gmail.com>
2024-10-12 12:19:03 +0200

    title change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -50,7 +50,7 @@ function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 export const metadata: Metadata = {
   title: 'About',
   description:
-    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+    'I am Diego, a Fullstack Engineer',
 }
 
 export default function About() {
